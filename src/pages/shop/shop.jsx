1c82eb8f437ef83825189a967a5479a953a5d579
02arslon <arslonruziboyev@gmail.com>
2023-06-02 19:25:26 +0900

    changed

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Shop() {
-  return <div>Sjop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Aslan Shop</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
