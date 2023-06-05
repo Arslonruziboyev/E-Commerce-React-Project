5247cdb672f893df336d616ee2472c6d52df4f56
02arslon <arslonruziboyev@gmail.com>
2023-06-05 11:31:17 +0900

    changed

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
-  return <div>{productName}</div>;
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
+        <p>{productName}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
