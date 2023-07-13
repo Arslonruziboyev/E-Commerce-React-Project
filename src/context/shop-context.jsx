eebd384fb1710a4cf4a2180c3e1cc828a1391c66
02arslon <arslonruziboyev@gmail.com>
2023-07-13 11:07:12 +0900

    changed

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -11,8 +11,13 @@ const getDefaultCart = () => {
 export const ShopContext = createContext(null);
 export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
+
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
   return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
