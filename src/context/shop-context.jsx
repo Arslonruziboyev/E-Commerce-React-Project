3d5f6d9ff93246578052c9a1ccefc27a28a2a933
02arslon <arslonruziboyev@gmail.com>
2023-07-13 10:54:55 +0900

    changed

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,5 +1,7 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 export const ShopContext = createContext(null);
-export const ShopContextProvider = () => {
-  return <div></div>;
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState();
+
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
