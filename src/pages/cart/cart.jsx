7a2231dc8486a1cbb7a04fb39353ab8c4fc6e33a
02arslon <arslonruziboyev@gmail.com>
2023-07-13 16:46:11 +0900

    changed

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
 
 function Cart() {
-  return <div>Cart</div>;
+  const { cartItem } = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItem">
+        {PRODUCTS.map((product) => {
+          if (cartItem[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
