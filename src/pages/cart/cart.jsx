2813dfa09c38b6dbbdda19d92ee1fa2e413b270f
02arslon <arslonruziboyev@gmail.com>
2023-07-16 18:51:17 +0900

    changed

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,10 +3,12 @@ import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
 import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className="cart">
       <div>
@@ -19,11 +21,15 @@ const Cart = () => {
           }
         })}
       </div>
-      <div className="checkout">
-        <p>Subtotal: ${totalAmount}</p>
-        <button>Continue Shopping</button>
-        <button>Checkout</button>
-      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your Card is Empty</h1>
+      )}
     </div>
   );
 };
